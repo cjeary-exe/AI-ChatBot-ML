222dda355e239a002ece5cadcb5a6bf89cd37917
CJeary <main@corbynjeary.com>
2025-08-06 14:44:38 +0100

    Setup model and trained it, dumping to model.pkl using py-pickle

---
+++ train.py
@@ -0,0 +1,49 @@
+import json
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+import pickle
+
+# Open the json file in read mode and parse its data to the variable intents
+
+jsonFile = open("intents.json", "r")
+intents = json.load(jsonFile)
+
+# Create the pattern / responses list
+
+X = []
+y = []
+
+# Populate X and y
+
+for intent_name, intent_data in intents.items():
+    for intent_pattern in intent_data['patterns']:
+        X.append(intent_pattern)
+        y.append(intent_name)
+
+# Import vectorizer
+
+vectorizer = TfidfVectorizer()
+
+# Learn vocab and IDF from the training sentences
+
+vectorizer.fit(X)
+
+# Convert training sentences into numeric vectors
+
+X_vectors = vectorizer.transform(X)
+
+# Import model
+
+model = LogisticRegression()
+model.fit(X_vectors, y)
+
+# Check model accuracy
+
+print("Accuracy:", accuracy_score(y, model.predict(X_vectors)))
+
+# Create save file and dump model into it using pickle
+
+file = open("model.pkl", "wb")
+pickle.dump(model, file)
